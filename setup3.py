8b5ab2b1210e773e02e1bd6236613865cd5b9129
Jesus Cea <jcea@jcea.es>
2017-09-08 19:29:42 +0200

    Document that Python 3.6 is actually supported

--- setup3.py
+++ setup3.py
@@ -546,6 +546,7 @@ pybsddb_doc/>`__ --
                     'Programming Language :: Python :: 3.3',
                     'Programming Language :: Python :: 3.4',
                     'Programming Language :: Python :: 3.5',
+                    'Programming Language :: Python :: 3.6',
                     ],
       )
 
