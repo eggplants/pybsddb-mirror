9c711ca892ad0aaf45e028e2b7042914c3cfe439
Jesus Cea <jcea@jcea.es>
2020-11-26 16:54:46 +0100

    6.2.9
    --HG--
    branch : bsddb3

--- docs/conf.py
+++ docs/conf.py
@@ -65,9 +65,9 @@ copyright = '2008-2020 Jesus Cea Avion'
 # The default replacements for |version| and |release|.
 #
 # The short X.Y version.
-version = '6.2.8'
+version = '6.2.9'
 # The full version, including alpha/beta/rc tags.
-release = '6.2.8'
+release = '6.2.9'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
